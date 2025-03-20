e5e17745c7f1b98e79f209748d1b31e0108aecb4
Chetan Rathod <chetan0101career@gmail.com>
2025-03-20 23:24:13 +0530

    erp project setup settings, urls, templates, statics etc

---
+++ adminApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('signin', views.signin, name='signin'),
+    path('', views.dashboard, name='dashboard'),
+]
--- adminApp/views.py
+++ adminApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signin(request):
+    return render(request, 'signin.html')
+
+
+
+def dashboard(request):
+    return render(request, 'base.html')
